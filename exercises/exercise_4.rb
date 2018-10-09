88bab80a304ea3ac0150f2732fb820c70b5a1004
paulabarreto <paulavilaca@gmail.com>
2018-10-09 22:18:32 +0000

    4 fixed and 5 completed

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -14,11 +14,11 @@ Store.create(name:"Yaletown", annual_revenue: 430000, mens_apparel: true, womens
 
 @mens_stores = Store.where(mens_apparel: "true")
 
-@mens_stores.each { |store| puts store.name, store.annual_revenue }
+# @mens_stores.each { |store| puts " Men's apparel stores and revenues: #{store.name}, #{store.annual_revenue}" }
 
 # Do another fetch but this time load stores that carry women's
 # apparel and are generating less than $1M in annual revenue.
 
-@womens_stores = Store.where('womens_apparel=?, annual_revenue<?',true, 1000000)
+@womens_stores = Store.where("womens_apparel=? and mens_apparel=? and annual_revenue<?",true, false, 1000000)
 
-@mens_stores.each { |store| puts store.name }
+# @womens_stores.each { |store| puts "Stores that only sell women's apparel and have less than 1M annual revenue: #{store.name}" }
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+sum = Store.sum(:annual_revenue)
+
+# puts "Sum: #{sum}"
+
+avg = sum / 5
+
+# puts "Average: #{avg}"
+
+@rich_stores = Store.where("annual_revenue>?", 1000000).count
+
+puts @rich_stores
