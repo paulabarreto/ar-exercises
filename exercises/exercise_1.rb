f45ef9b9720d38433c51ee60c4aea5884cb1f488
paulabarreto <paulavilaca@gmail.com>
2018-10-09 21:45:33 +0000

    Exercises 1-4 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -14,12 +14,3 @@ Store.create(name:"Richmond", annual_revenue: 1260000, mens_apparel: false, wome
 Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts Store.count
-
-# user = User.create(name: "David", occupation: "Code Artist")
-
-
-# t.column :name, :string
-# t.column :annual_revenue, :integer
-# t.column :mens_apparel, :boolean
-# t.column :womens_apparel, :boolean
-# t.timestamps null: false
