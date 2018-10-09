4f85adde56a3889b25e0d691f6424781cab9148c
paulabarreto <paulavilaca@gmail.com>
2018-10-09 20:42:46 +0000

    exercise 1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,22 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+
+end
+
+Store.create(name:"Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
+Store.create(name:"Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name:"Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
+
+# user = User.create(name: "David", occupation: "Code Artist")
+
+
+# t.column :name, :string
+# t.column :annual_revenue, :integer
+# t.column :mens_apparel, :boolean
+# t.column :womens_apparel, :boolean
+# t.timestamps null: false
